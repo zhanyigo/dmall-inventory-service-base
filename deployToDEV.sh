4d14472a2b876a3d7d1fa63b53a3c1b3e8870ce9
占翼 <13795260@qq.com>
2018-02-08 15:35:15 +0800

    fix start script

--- deployToDEV.sh
+++ deployToDEV.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
 kubectl --kubeconfig=$KUBECTL_CONFIG_FILE delete -f inventory-service.yaml
+kubectl --kubeconfig=$KUBECTL_CONFIG_FILE delete -f inventory-service.yaml
+
+sleep 3
 kubectl --kubeconfig=$KUBECTL_CONFIG_FILE create -f inventory-service.yaml
