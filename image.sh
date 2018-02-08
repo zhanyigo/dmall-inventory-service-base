4bdb213db710bb35e2cdd8824556cb0d521210be
占翼 <13795260@qq.com>
2018-02-08 14:39:45 +0800

    修改镜像脚本

--- image.sh
+++ image.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-DMALL_DOCKER_REGISTRY="ec2-54-95-48-23.ap-northeast-1.compute.amazonaws.com:5000"
+DMALL_DOCKER_REGISTRY='ec2-54-95-48-23.ap-northeast-1.compute.amazonaws.com:5000'
 SLUG='zhanyi'
+
 sudo docker build -t $DMALL_DOCKER_REGISTRY/$SLUG/inventory-service:$BUILD_NUMBER .
 sudo docker tag $DMALL_DOCKER_REGISTRY/$SLUG/inventory-service:$BUILD_NUMBER $DMALL_DOCKER_REGISTRY/$SLUG/inventory-service:latest
 
